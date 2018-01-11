e28a32b92428d09522c3abff61eb07a85557d682
liuwei53 <liuwei53@baidu.com>
2018-01-11 14:37:35 +0800

    购物车部分

--- pages/product/product.js
+++ pages/product/product.js
@@ -1,7 +1,9 @@
 // pages/product/product.js
 
 import { Product } from 'product-model.js';
-var product = new Product();
+import { Cart } from '../cart/cart-model.js';
+var product = new Product();  //实例化 商品详情 对象
+var cart = new Cart();
 
 Page({
 
@@ -28,7 +30,8 @@ Page({
     var that = this;
     product.getDetailInfo(this.data.id, (data) => {
       that.setData({
-        product: data
+        product: data,
+        cartTotalCounts: cart.getCartTotalCounts().counts1
       });
       callback && callback();
     });
@@ -53,6 +56,83 @@ Page({
       currentTabsIndex: index
     });
   },
+
+  /*添加到购物车*/
+  onAddingToCartTap: function (events) {
+    // 防止快速点击
+    if (this.data.isFly) {
+      return;
+    }
+    this._flyToCartEffect(events);
+    this.addToCart();
+  },
+
+
+  /*加入购物车动效*/
+  _flyToCartEffect: function (events) {
+    //获得当前点击的位置，距离可视区域左上角
+    var touches = events.touches[0];
+    var diff = {
+      x: '25px',
+      y: 25 - touches.clientY + 'px'
+    },
+      style = 'display: block;-webkit-transform:translate(' + diff.x + ',' + diff.y + ') rotate(350deg) scale(0)';  //移动距离
+    this.setData({
+      isFly: true,
+      translateStyle: style
+    });
+    var that = this;
+    setTimeout(() => {
+      that.setData({
+        isFly: false,
+        translateStyle: '-webkit-transform: none;',  //恢复到最初状态
+        isShake: true,
+      });
+      setTimeout(() => {
+        var counts = that.data.cartTotalCounts + that.data.productCounts;
+        that.setData({
+          isShake: false,
+          cartTotalCounts: counts
+        });
+      }, 200);
+    }, 1000);
+  },
+
+  /*将商品数据添加到内存中*/
+  addToCart: function () {
+    var tempObj = {}, 
+        keys = ['id', 'name', 'main_img_url', 'price'];
+    for (var key in this.data.product) {
+      if (keys.indexOf(key) >= 0) {
+        tempObj[key] = this.data.product[key];
+      }
+    }
+
+    cart.add(tempObj, this.data.productCounts);
+
+  },
+
+  /*跳转到购物车*/
+  onCartTap: function () {
+    wx.switchTab({
+      url: '/pages/cart/cart'
+    });
+  },
+
+  /*下拉刷新页面*/
+  onPullDownRefresh: function () {
+    this._loadData(() => {
+      wx.stopPullDownRefresh()
+    });
+  },
+
+  //分享效果
+  onShareAppMessage: function () {
+    return {
+      title: '零食商贩 Pretty Vendor',
+      path: 'pages/product/product?id=' + this.data.id
+    }
+  }
 
   
 })
